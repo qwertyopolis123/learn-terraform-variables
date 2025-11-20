7e975aaec4d4eab276fc3d122d2156d95989c66e
TEK Student <TEKstudent@vlab.lan>
2025-11-20 07:08:52 -0800

    Remove cloud block

--- main.tf
+++ main.tf
@@ -109,8 +109,8 @@ module "elb_http" {
 module "ec2_instances" {
   source = "./modules/aws-instance"
 
-  instance_count     = 2
-  instance_type      = "t2.micro"
+  instance_count = var.instance_count
+  instance_type  = var.instance_type
   subnet_ids         = module.vpc.private_subnets[*]
   security_group_ids = [module.app_security_group.this_security_group_id]
 
